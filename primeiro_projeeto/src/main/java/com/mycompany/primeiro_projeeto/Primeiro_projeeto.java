a2036ad5ba6d97149464ff82660483b1701c51e1
Felipe-Bignoto <felipebignoto@gmail.com>
2023-04-01 19:13:28 -0300

    primeiro commit

---
+++ primeiro_projeeto/src/main/java/com/mycompany/primeiro_projeeto/Primeiro_projeeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiro_projeeto;
+
+/**
+ *
+ * @author felip
+ */
+public class Primeiro_projeeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
